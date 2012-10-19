ee77ebca649835323505441987e5a5ac93855239
Vineet <naikvin@gmail.com>
2012-10-19 22:59:43 +0530

    Add a script to update gh-pages branch in another clone of the repo

---
+++ updateghpages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+REPO_PATH=/home/vineet/javascript/jquery-sticky-buttons
+GHPAGES_PATH=/home/vineet/javascript/ghpageclones/jquery-sticky-buttons
+
+cp $REPO_PATH/example/index.html $GHPAGES_PATH/
+cp $REPO_PATH/example/app.js $GHPAGES_PATH/
+cp $REPO_PATH/example/styles.css $GHPAGES_PATH/
+cp $REPO_PATH/jquery.stickybuttons.js $GHPAGES_PATH/
